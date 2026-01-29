19af7d72c97e8be5e529851bde2ed92fc87954ea
neutrono1 <shubhajitkunduboost02@gmail.com>
2026-01-29 18:11:58 +0530

    Initial commit: RAG-based AI teaching assistant project structure

---
+++ download_videos_YT.py
@@ -0,0 +1,44 @@
+import yt_dlp
+import os
+
+def download_playlist_videos():
+    playlist_url = "https://www.youtube.com/playlist?list=PLBlnK6fEyqRhG6s3jYIU48CqsT5cyiDTO"
+    download_folder = "VIDEOS"
+
+    os.makedirs(download_folder, exist_ok=True)
+
+    ydl_opts = {
+        # File naming
+        "outtmpl": f"{download_folder}/%(playlist_index)s - %(title)s.%(ext)s",
+                    # .ext means the file extension 
+        # Reliable format selection
+        "format": "bv*+ba/b",
+
+        # Download range
+        "playlist_items": "1-15",
+
+        # Merge
+        "merge_output_format": "mp4",
+
+        # ✅ JS runtime (correct format)
+        "js_runtimes": {
+            "node": {}
+        },
+
+        # Stability options
+        "retries": 10,
+        "fragment_retries": 10,
+        "sleep_interval": 1,
+        "max_sleep_interval": 3,
+
+        # Less noise, still informative
+        "quiet": False,
+        "no_warnings": False,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([playlist_url])
+
+    print("✅ Downloaded first 15 videos successfully")
+
+download_playlist_videos()
---
+++ video_to_mp3.py
@@ -0,0 +1,19 @@
+import os 
+import subprocess
+
+files = os.listdir("VIDEOS")
+os.makedirs("AUDIOS", exist_ok=True)
+
+for file in files :
+   print(file)
+   tutorial_number = file.split(" - ")[0]
+   file_name = file.split(" - ")[1].split(".")[0]
+   #print(f"tutorial_number: {tutorial_number}, file_name: {file_name}")
+   subprocess.run([
+        "ffmpeg",
+        "-nostdin",      # 🔑 THIS IS THE FIX
+        "-y",
+        "-i", f"Signals_and_Systems/{file}",
+        f"AUDIOS/{tutorial_number}_{file_name}.mp3"
+    ]
+    )
